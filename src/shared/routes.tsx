5c8721f8fe77538e4a8eaf6341b7c74f1e5143e2
ReshadSadik <reshadsadik@gmail.com>
2023-09-11 00:16:25 +0600

    first commit

---
+++ src/shared/routes.tsx
@@ -0,0 +1,9 @@
+export const ALL_ROUTES = {
+  REGISTER: `/register`,
+  LOGIN: `/login`,
+  HOME: '/',
+  UPLOAD_DOC: '/upload-doc',
+  DOC_LIST: '/bots',
+  CHAT: '/chat/:botId',
+  MY_PROFILE: '/my-profile',
+};
